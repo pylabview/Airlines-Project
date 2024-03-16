4d64e781c2e0b341f8889a2767bbda7a852a27da
jsphilippe <157438908+jsphilippe@users.noreply.github.com>
2024-03-16 19:54:10 -0400

    ...

--- WPI-ATR/backend/src/main/java/com/wpiatr/backend/repository/AirportRepository.java
+++
@@ -1,8 +0,0 @@
-package com.wpiatr.backend.repository;
-
-import com.wpiatr.backend.model.Airport;
-import org.springframework.data.jpa.repository.*;
-
-public interface AirportRepository extends JpaRepository<Airport, Integer> {
-
-}
--- WPI-ATR/backend/src/main/java/com/wpiatr/backend/repository/AirportsRepository.java
+++
@@ -1,5 +0,0 @@
-package com.wpiatr.backend.repository;
-import com.wpiatr.backend.model.Airports;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface AirportsRepository extends JpaRepository<Airports, Integer> {
-}
