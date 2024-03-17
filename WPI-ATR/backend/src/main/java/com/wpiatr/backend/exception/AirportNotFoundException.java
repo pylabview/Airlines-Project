5f3946d9025c3c56436612b301ece8fd65bcb4a8
jsphilippe <157438908+jsphilippe@users.noreply.github.com>
2024-03-17 19:04:30 -0400

    Delete WPI-ATR directory

--- WPI-ATR/backend/src/main/java/com/wpiatr/backend/exception/AirportNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.wpiatr.backend.exception;
-
-public class AirportNotFoundException extends RuntimeException {
-    public AirportNotFoundException(Integer id_airport){
-        super("Could not find airport id: " + id_airport);
-    }
-}
